a57fc80491e3e0f465696976c18c5e4b7602164b
DavidMS <davidmartinezsepulveda@gmail.com>
2021-03-02 08:58:23 +0100

    initial commit

---
+++ src/test/java/AlarmTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+
+public class AlarmTest {
+
+    @Test
+    public void foo() {
+        Alarm alarm = new Alarm();
+        assertFalse(alarm.isAlarmOn());
+    }
+}
